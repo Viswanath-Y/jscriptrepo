609efd317bd6b96e0d6a815c704da32d7e5834c8
Viswanath-Y <53591420+Viswanath-Y@users.noreply.github.com>
2019-08-02 10:24:42 +0530

    Create placementHtml.js

---
+++ placementHtml.js
@@ -0,0 +1,12 @@
+<html>
+<head>
+<script language="javascript" type="type/javascript">
+  function sayHelloWorld(){
+  alert("Hellow World");
+  }
+</script>
+</head>
+<body>
+<input type="button" onclick="sayHelloWorld()" value="sayHelloWorld"/>
+</body>
+</html>
